1817a02af0d50fbc8dcd81246a1b114fdad24065
kasiaosinska <katarzynabosinska@gmail.com>
2017-10-21 17:38:58 +0200

    Show logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'intersection-observer'; // optional polyfill
+import Observer from '@researchgate/react-intersection-observer';
 
 
 import Block from './components/Block';
@@ -11,26 +13,41 @@ class App extends Component {
         super(props);
     }
 
-    // checkIfIsVisible = () => {
-    //     let div = document.querySelectorAll('.myDiv')[0];
-    //     let height = div.getBoundingClientRect().top;
-    //
-    //     console.log(height);
-    // };
 
-    // document.getElementsById('.App').appendChild(<Logo style="display:block"/>)
+    handleIntersection(event) {
+        console.log(event.isIntersecting);
+        console.log("I'm in viewport");
+
+        let logo = document.querySelector('.logo');
+
+        if(event.isIntersecting) {
+            logo.style.display = 'block';
+        }
+        else {
+            logo.style.display = 'none';
+        }
+
+    }
 
 
     render() {
+
+        const options = {
+            onChange: this.handleIntersection,
+            root: null,
+            rootMargin: "0px",
+            threshold: 0
+        };
+
         return (
-          <div classID="App">
-            <Block color={'green'} />
-            <Block color={'green'} />
-            <Block color={'blue'} />
-            <Block color={'green'} />
-            <Logo/>
-            <Jokes />
-          </div>
+            <div classID="App">
+                <Logo/>
+                <Block color={'green'} />
+                <Block color={'green'} />
+                <Observer {...options}>
+                    <Block color={'blue'} />
+                </Observer>
+            </div>
         );
     }
 }
